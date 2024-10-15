ea80f2cac9ffef58362515f245232e8e765f31be
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-10-16 04:42:05 +0600

    Before fojor salat

--- src/component/BackgroundVideoComponent.jsx
+++ src/component/BackgroundVideoComponent.jsx
@@ -22,7 +22,11 @@ const BackgroundVideoComponent = () => {
       </video>
 
       {/* Overlay content on hover */}
-      
+      {isHovered && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <h1 className="text-white text-4xl md:text-5xl lg:text-6xl font-bold">Welcome To Aplura</h1>
+        </div>
+      )}
     </div>
   );
 };

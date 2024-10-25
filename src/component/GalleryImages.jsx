a0ea07c604084b37f109af7c5b139bbd2ab5f4c2
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2024-10-25 17:02:15 +0600

    responsiveness of GalleryImage

--- src/component/GalleryImages.jsx
+++ src/component/GalleryImages.jsx
@@ -54,31 +54,31 @@ const GalleryImages = () => {
         <div
           className={`flex flex-col md:flex-row ${
             index % 2 === 0 ? 'md:flex-row-reverse' : ''
-          } min-h-[350px] mb-10`}
+          } md:min-h-[350px] mb-10`}
           key={index}
         >
           {/* Image Section */}
-          <div className="relative flex-[2] h-[300px] md:h-[400px] overflow-hidden">
+          <div className="relative flex md:flex-[2] h-[200px] md:h-[400px] overflow-hidden w-full">
             <div
-              className="bg-cover bg-center bg-fixed w-full h-full rounded-md"
+              className="bg-cover bg-center w-full h-full rounded-md"
               style={{
                 backgroundImage: `url(${service.imageUrl})`,
-                backgroundAttachment: 'fixed', // Keep the background fixed
+                backgroundAttachment: window.innerWidth >= 768 ? 'fixed' : 'scroll', // Conditional backgroundAttachment
               }}
             ></div>
 
             {/* Title over image */}
             <div className="absolute inset-0 flex items-center justify-center">
-              <h2 className="text-white text-2xl md:text-4xl font-bold bg-black bg-opacity-50 px-4 py-2 rounded-md w-[70%]">
+              <h2 className="text-white text-2xl md:text-3xl font-bold bg-black bg-opacity-50 px-4 py-2 rounded-md w-[90%] md:w-[70%] text-center">
                 {service.title}
               </h2>
             </div>
           </div>
 
           {/* Text Section */}
-          <div className="flex-[1] flex flex-col justify-center p-6 md:p-10 bg-white rounded text-secondary">
-            <h3 className="text-2xl font-bold mb-2">{service.heading}</h3>
-            <p className="text-lg">{service.text}</p>
+          <div className="flex md:flex-[1] flex-col justify-center p-4 md:p-10 bg-white rounded text-secondary w-full">
+            <h3 className="text-xl md:text-2xl font-bold mb-2">{service.heading}</h3>
+            <p className="text-base md:text-lg">{service.text}</p>
           </div>
         </div>
       ))}
@@ -87,3 +87,4 @@ const GalleryImages = () => {
 };
 
 export default GalleryImages;
+  

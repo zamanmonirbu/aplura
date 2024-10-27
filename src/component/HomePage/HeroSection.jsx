c850d4ebbe0bad1e248ede254d71ef237e5f4d3f
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-10-28 01:49:26 +0600

    fine tunning

--- src/component/HomePage/HeroSection.jsx
+++ src/component/HomePage/HeroSection.jsx
@@ -19,14 +19,14 @@ const ImageUpDown = () => {
 
       {/* Scrollable Content */}
       <div className="relative z-10 flex flex-col justify-start items-center h-full p-4 overflow-auto sm:h-[90vh] md:h-[80vh] lg:h-[70vh]">
-        <div className="into flex flex-col items-center h-full mt-[7%] md:mt-[105px]">
+        <div className="into flex flex-col items-center h--full mt-[7%] md:mt-[105px]">
           <p className="text-[#DDE0E0] text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-center uppercase">
             Innovation Starts in the Soil
           </p>
-          <p className="text-[#F9F9F9] text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center my-4 md:mb-2">
+          <p className="text-[#F9F9F9] text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center my-4">
             Your Success
           </p>
-          <p className="text-[#FFFAFA] text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-bold md:mt-[40px]">
+          <p className="text-[#FFFAFA] text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-bold md:mt-[40px] md:mb-[25px]">
             Our Best Yield
           </p>
         </div>

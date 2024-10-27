bdbf55df3d1b47152b1e958a48983834e2cd72f0
Md. Rahat <rahat.cse5.bu@gmail.com>
2024-10-28 01:08:18 +0600

    support page fixed

--- src/component/GalleryImages.jsx
+++ src/component/GalleryImages.jsx
@@ -67,7 +67,7 @@ const GalleryImages = () => {
             ></div>
 
             {/* Title over image */}
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30">
               <h2 className="text-white text-xl md:text-2xl font-bold px-4 py-2 rounded-md w-[90%] md:w-[70%] text-center">
                 {service.title}
               </h2>
